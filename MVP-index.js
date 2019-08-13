c1f3eca22e6679fdbfc7efa65149cd444622a24e
Froglegg <hayescrowley@gmail.com>
2019-08-13 18:33:41 -0400

    letter is working, sort of

--- MVP-index.js
+++ MVP-index.js
@@ -5,79 +5,76 @@ const inquirer = require('inquirer');
 const userInput = process.argv[2];
 const chalkPipe = require('chalk-pipe');
 
-function app (){ 
-// declaring some variables...
-let wordList = ["awesome", "great", "terrific", "wonderful", "outstanding", "marvelous"];
-let answerArray = [];
-let guessedList = [];
-var randomWord;
+function app() {
+    // declaring some variables...
+    let wordList = ["awesome", "great", "terrific", "wonderful", "outstanding", "marvelous"];
+    let answerArray = [];
+    let guessedList = [];
+    var randomWord;
 
-// random word function... storing it as a function in a variable for some reason?
-var randomWord = function() {
-    return randomWord = wordList[Math.floor(Math.random() * wordList.length)].split('');
-}
-// calling random word function 
-randomWord();
+    // random word function... storing it as a function in a variable for some reason?
+    var randomWord = function() {
+            return randomWord = wordList[Math.floor(Math.random() * wordList.length)].split('');
+        }
+        // calling random word function 
+    randomWord();
 
-console.log(`calling random word function on line 22 word is ${randomWord}`);
+    console.log(`calling random word function on line 22 word is ${randomWord}`);
 
-// creating spaces that match the length of the random word, adding them to the answer array which gets logged after every inquirer prompt is answered
-for (var i = 0; i < randomWord.length; i++) {
-    answerArray[i] = "_";
-}
-
-function guesser() {
-    // make sure the guess hasn't been made already, if so, create new random word, restarts app 
-    if(answerArray.join("") == randomWord.join("")){
-        console.log(chalkPipe('green.bold')(`Correct! You guessed it!`));
-        app();
+    // creating spaces that match the length of the random word, adding them to the answer array which gets logged after every inquirer prompt is answered
+    for (var i = 0; i < randomWord.length; i++) {
+        answerArray[i] = "_";
     }
-    else{
-    inquirer.prompt([{
-        type: "input",
-        name: "input",
-        message: "Guess a Letter!",
-        validate: function(value) {
-            var pass = value.match(
-                /^[a-zA-Z]$/
-            );
-            if (pass) {
-                return true;
-            }
 
-            return chalkPipe('red.bold')('Please enter a single letter');
-        }
-    }]).then(answer => {
-        if (guessedList.includes(answer.input)){
-            console.log(chalkPipe('blue.bold')(`You already guessed ${answer.input}`));
-        }
-        else {
-            guessedList.push(answer.input);
-            console.log(`Guess: ${answer.input}`);
-            console.log(`Letters guessed: ${guessedList}`);   
-            if (randomWord.includes(answer.input)) {
-                console.log(chalkPipe('green.bold')('Correct!'));
-                for (var i = 0; i < randomWord.length; i++) {
-                    if (randomWord[i] === answer.input) {
-                        answerArray[i] = answer.input;
+    function guesser() {
+        // make sure the guess hasn't been made already, if so, create new random word, restarts app 
+        if (answerArray.join("") == randomWord.join("")) {
+            console.log(chalkPipe('green.bold')(`Correct! You guessed it!`));
+            app();
+        } else {
+            inquirer.prompt([{
+                type: "input",
+                name: "input",
+                message: "Guess a Letter!",
+                validate: function(value) {
+                    var pass = value.match(
+                        /^[a-zA-Z]$/
+                    );
+                    if (pass) {
+                        return true;
+                    }
+
+                    return chalkPipe('red.bold')('Please enter a single letter');
+                }
+            }]).then(answer => {
+                if (guessedList.includes(answer.input)) {
+                    console.log(chalkPipe('blue.bold')(`You already guessed ${answer.input}`));
+                } else {
+                    guessedList.push(answer.input);
+                    console.log(`Guess: ${answer.input}`);
+                    console.log(`Letters guessed: ${guessedList}`);
+                    if (randomWord.includes(answer.input)) {
+                        console.log(chalkPipe('green.bold')('Correct!'));
+                        for (var i = 0; i < randomWord.length; i++) {
+                            if (randomWord[i] === answer.input) {
+                                answerArray[i] = answer.input;
+                            }
+                        }
+                    } else {
+                        console.log(chalkPipe('red.bold')('Wrong!'));
                     }
                 }
-            } else 
-            {
-                console.log(chalkPipe('red.bold')('Wrong!'));
-            } 
+
+                // console.log(randomWord, guessedList, randomWord.some(v => guessedList.includes(v)));
+
+                console.log(`${answerArray.join(" ")}`);
+
+                guesser();
+            });
         }
-
-        // console.log(randomWord, guessedList, randomWord.some(v => guessedList.includes(v)));
-
-        console.log(`${answerArray.join(" ")}`);
-
-        guesser();
-    });
-}
-}
-// starts the inquirer prompt, unless word has been guessed, then it will restart the app 
-guesser();
+    }
+    // starts the inquirer prompt, unless word has been guessed, then it will restart the app 
+    guesser();
 }
 
 // starts the application
--- letter.js
+++ letter.js
@@ -1,37 +1,50 @@
-const Letter = function(char, guessed) {
-
+const Letter = function(char) {
     this.char = char;
     this.guessed = false;
-    this.checkGuess = function(char) {
-        if (this.guessed === true) {
-            guess === false;
-            return char;
+
+    this.checkGuess = function(charGuess) {
+        if (charGuess == char) {
+
+            return this.guessed = true;
         } else {
-            return "_";
+            return this.guessed = false;
         }
     }
+
+    this.validateGuess = function() {
+        if (this.guessed = false) {
+            return this.char = "_"
+        } else {
+            return this.char = char;
+        }
+    }
+
 }
+
+var letter = new Letter("i");
+
+console.log(letter.checkGuess("h"));
 
 module.exports = Letter;
 
 // https: //jsbin.com/facawetume/edit?js,console
 
-    // * **Letter.js**: Contains a constructor, Letter. This constructor should be able to either display an underlying character or a blank placeholder (such as an underscore), depending on whether or not the user has guessed the letter. That means the constructor should define:
+// * **Letter.js**: Contains a constructor, Letter. This constructor should be able to either display an underlying character or a blank placeholder (such as an underscore), depending on whether or not the user has guessed the letter. That means the constructor should define:
 
-    // * A string value to store the underlying character for the letter
+// * A string value to store the underlying character for the letter
 
-    // * A boolean value that stores whether that letter has been guessed yet
+// * A boolean value that stores whether that letter has been guessed yet
 
-    // * A function that returns the underlying character if the letter has been guessed, or a placeholder (like an underscore) if the letter has not been guessed
+// * A function that returns the underlying character if the letter has been guessed, or a placeholder (like an underscore) if the letter has not been guessed
 
-    // * A function that takes a character as an argument and checks it against the underlying character, updating the stored boolean value to true if it was guessed correctly
+// * A function that takes a character as an argument and checks it against the underlying character, updating the stored boolean value to true if it was guessed correctly
 
-    // randomWord = wordList[Math.floor(Math.random() * wordList.length)];
-    // getHint = wordList.indexOf(randomWord);
-    // // console.log(getHint);
-    // hint = hintList[getHint];
-    // // console.log(hint);
-    // for (var i = 0; i < randomWord.length; i++) {
-    //     answerArray[i] = "_";
-    // };
-    // space = answerArray.join(" ");
+// randomWord = wordList[Math.floor(Math.random() * wordList.length)];
+// getHint = wordList.indexOf(randomWord);
+// // console.log(getHint);
+// hint = hintList[getHint];
+// // console.log(hint);
+// for (var i = 0; i < randomWord.length; i++) {
+//     answerArray[i] = "_";
+// };
+// space = answerArray.join(" ");
